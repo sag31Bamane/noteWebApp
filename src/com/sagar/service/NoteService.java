3bf5782f7fba7fef8c2f923234d1ed08b061ea03
unknown <sagar31bamane@yahoo.com>
2016-03-27 23:12:46 +0530

    adding json library

--- src/com/sagar/service/NoteService.java
+++ src/com/sagar/service/NoteService.java
@@ -1,10 +1,15 @@
 package com.sagar.service;
 
+import java.util.ArrayList;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+import com.sagar.entity.User;
 
 @Path("noteService")
 public class NoteService{
@@ -18,4 +23,25 @@ public class NoteService{
 		return result;
 
 	}
+	
+	@GET
+	@Path("getUsers")
+	@Produces("application/json")
+	public String getUser(){
+		{
+			String feeds = null;
+			try
+			{
+			ArrayList<User> userData = null;
+			Gson gson = new Gson();
+			System.out.println(gson.toJson(userData));
+			feeds = gson.toJson(userData);
+			}
+
+			catch (Exception e)
+			{
+			System.out.println("Exception Error"); //Console 
+			}
+			return feeds;
 }
+
